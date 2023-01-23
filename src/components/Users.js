bb61bc1598458eb8860846fc2339ff91b73139ec
Your Name <rghzaiel@ggn-group.com>
2023-01-23 11:27:54 +0100

    class state

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { Component } from "react";
 import User from "./User";
 import classes from "./Users.module.css";
 
@@ -10,14 +10,31 @@ const DUMMY_USERS = [
 
 //Class based component
 class Users extends Component {
-  toggleUsersHandler() {}
+  constructor() {
+    super();
+    this.state = {
+      showUsers: false,
+    };
+  }
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
   render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
     return (
       <div className={classes.users}>
-        <button onClick={this.toggleUsersHandler}>
-          {showUsers ? "Hide" : "Show"} Users
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
         </button>
-        {showUsers && usersList}
+        {this.state.showUsers && usersList}
       </div>
     );
   }
